e185e50d211bf25b4ff26c1995c14c778cb7c2ad
tyler goodman <tylercgoodman@gmail.com>
2015-04-15 17:28:36 -0700

    separated protractor tests into its correct repo

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tangelo Protractor Tests
+
+Protractor tests for Tangelo's angular frontend.
+
+####How to run:
+
+1. Make sure Tangelo is running
+2. Run the following npm command:
+
+```bash
+npm test
+```
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var protractor = require('gulp-protractor').protractor;
+
+gulp.task('protractor', function () {
+  gulp.src(['./specs/**/*.js'])
+    .pipe(protractor({
+      configFile: './protractor-config.js',
+      verbose: false,
+      includeStackTrace: false
+        // args: ['--verbose', 'false',]
+    }))
+    .on('error', function (e) {
+      throw e
+    });
+});
+
+gulp.task('test', function (done) {
+  runSequence('protractor', done);
+});
+
+gulp.task('default', function (done) {
+  runSequence('test', done);
+});
